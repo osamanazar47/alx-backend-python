97319043a8dfeaf47b5b22fd78065f80e132e57f
osamanazar47 <osamanazar47@gmail.com>
2024-07-08 12:56:06 +0300

    annotating all the first task functions and coroutines

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int=10) -> float:
   """A coroutines that awaits for a random delay and returns it"""
-  delay = random.uniform(0, max_delay)
+  delay: float = random.uniform(0, max_delay)
   await asyncio.sleep(delay)
   return delay
