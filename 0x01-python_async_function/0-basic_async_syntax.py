5035edc7d2bee1515babedf602946c25333e4faf
osamanazar47 <osamanazar47@gmail.com>
2024-07-08 12:59:16 +0300

    fixing style in task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""module for the first task about the basic async usage.
+""" module for the first task about the basic async usage.
 """
 import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
-  """A coroutines that awaits for a random delay and returns it"""
-  delay: float = random.uniform(0, max_delay)
-  await asyncio.sleep(delay)
-  return delay
+async def wait_random(max_delay: int = 10) -> float:
+    """A coroutines that awaits for a random delay and returns it"""
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
