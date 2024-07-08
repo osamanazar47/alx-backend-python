f042a1ec11e410ad7b4a38db2f9713640eb32d5e
osamanazar47 <osamanazar47@gmail.com>
2024-07-08 13:27:40 +0300

    imported List from typing module

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ returns a list of radom ascending delay values"""
-    delay_list: list[float] = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    delay_list = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
     return sorted(delay_list)
