871d7c3549a1f35b2766323821ef35dbfed4b7bd
osamanazar47 <osamanazar47@gmail.com>
2024-07-08 13:25:14 +0300

    finihed task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A module for task 1
+task for executing multiple coroutines
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """ returns a list of radom ascending delay values"""
+    delay_list: list[float] = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return sorted(delay_list)
