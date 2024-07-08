86a97e367bb470275db3634e2ff54b97ac7cf795
osamanazar47 <osamanazar47@gmail.com>
2024-07-08 13:29:04 +0300

    fixed style issue in task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,5 +11,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ returns a list of radom ascending delay values"""
-    delay_list = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
-    return sorted(delay_list)
+    delay_l = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return sorted(delay_l)
