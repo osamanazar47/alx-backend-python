c2903fb489430d5597d05a0dfb073d8524008fe2
osamanazar47 <osamanazar47@gmail.com>
2024-07-09 21:08:37 +0300

    finished the third task style issues

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """coroutine that will execute async_comprehension 4 times in parallel"""
     start = time.time()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
     end_time = time.time()
