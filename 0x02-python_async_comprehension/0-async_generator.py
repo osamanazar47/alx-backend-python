81e62a6bc157fd9395871d8b084e2d50e10b0b10
osamanazar47 <osamanazar47@gmail.com>
2024-07-09 20:52:25 +0300

    the first task fixing style

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float,None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """a corountine that loops 10 times"""
     for number in range(10):
         await asyncio.sleep(1)
