174c84b21dd08ecd7d17d8a6a5730a550a24d7b6
osamanazar47 <osamanazar47@gmail.com>
2024-07-09 21:07:09 +0300

    finished the third task

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for the third and last task"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = time.time()
+    total_time = end_time - start
+    return total_time
