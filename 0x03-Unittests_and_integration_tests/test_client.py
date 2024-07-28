38cd3171752606cca6f054fee94d89a108e388d9
osamanazar47 <osamanazar47@gmail.com>
2024-07-29 00:59:50 +0300

    task 34

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for testing the client.py file"""
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+from typing import Dict
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Difining the class for testing the client file"""
+    @parameterized.expand([
+		("google", {"login": "google"}),
+		("abc", {"login": "abc"}),
+	])
+    @patch('client.get_json')
+    def test_org(self, org: str, exp_result: Dict, mock_g_json: Mock) -> None:
+        """for testing the org method"""
+        mock_g_json.return_value = exp_result
+        client = GithubOrgClient(org)
+        result = client.org
+
+        exp_url = f"https://api.github.com/orgs/{org}"
+        mock_g_json.assert_called_once_with(exp_url)
+        self.assertEqual(result, exp_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
